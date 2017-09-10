7e0e3df0a5cf8764745da95d995deae9f3655abd
Marcelo Ferreira <marcelohpf@gmail.com>
2017-09-09 23:23:27 -0300

    Initial data laod and predict with Naive Bayers
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

---
+++ train.py
@@ -0,0 +1,60 @@
+import os
+import pandas
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_validate
+from sklearn.model_selection import cross_val_score
+
+
+# Load dirs name
+cur_dir = os.path.realpath('.')
+pos_dir = os.path.join(cur_dir, 'pos')
+neg_dir = os.path.join(cur_dir, 'neg')
+
+tfidfv = TfidfVectorizer(input='filename')
+
+# Load files names
+list_pos_dir = [ (os.path.join(pos_dir, x), 1) for x in os.listdir(pos_dir)][:2000]
+list_neg_dir = [ (os.path.join(neg_dir, x), 0) for x in os.listdir(neg_dir)][:2000]
+print("registers: {}".format(len(list_pos_dir+list_neg_dir)))
+print("Attention with 6000 registers it will consume about 5+GB of ram")
+input("Continue? or press CTRL+C")
+
+# Mount data with label data frame
+paths_df = pandas.DataFrame(list_pos_dir+list_neg_dir, columns=['path', 'label'])
+
+# Load text and transform into vector
+data = tfidfv.fit_transform(paths_df['path'].values)
+
+# Split into train and test
+X, x, Y, y = train_test_split(
+    data.toarray(), paths_df.label, test_size=0.3, random_state=0
+)
+
+#dataframe = pandas.DataFrame(data.toarray(), columns=tfidfv.get_feature_names())
+#print(paths_df, "\n")
+#print(paths_df['path'].head())
+#print(list_pos_dir, list_neg_dir)
+#print(tfidfv.get_feature_names(), len(tfidfv.get_feature_names()))
+#print(tfidfv)
+#print(dataframe.head())
+#print(data, paths_df['label'].values, len(paths_df['label'].values))
+#print(data.toarray())
+
+# gnb method
+def gnb():
+    gnb = GaussianNB()
+
+    # Complexy analysis
+    #result = cross_val_score(gnb, data.toarray(), paths_df.label.values, cv=2)
+    #print(result)
+    # Simple analysis
+    gnb.fit(X,Y)
+    score = gnb.score(x, y)
+    print("test",score)
+    score = gnb.score(X, Y)
+    print("train",score)
+
+gnb()
