a51a12bfa4a651d810edd71de7eb0aa27e474d13
Marcelo Ferreira <marcelohpf@gmail.com>
2017-09-10 19:01:51 -0300

    Add comparision with tokenizer
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

--- dense_transform.py
+++ dense_transform.py
@@ -1,13 +1,24 @@
-from sklearn.base import TransformerMixin
+from sklearn.base import TransformerMixin, BaseEstimator
+import nltk
 
-class DenseTransformer(TransformerMixin):
+stemmer = nltk.stem.snowball.SnowballStemmer('english')
+
+try:
+    nltk.word_tokenize('')
+except LookupError:
+    nltk.download('punkt')
+
+def tokenizer(text):
+    tokens = nltk.word_tokenize(text)
+    return [stemmer.stem(token) for token in tokens]
+        
+        
+
+
+class DenseTransformer(TransformerMixin, BaseEstimator):
     def transform(self, X, y=None, **fit_params):
         return X.todense()
 
     def fit_transform(self, X, y=None, **fit_params):
-        self.fit(X, y, **fit_params)
         return self.transform(X)
-
-    def fit(self, X, y=None, **fit_params):
-        return self
 
--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import os
 import pandas
 import numpy
-from dense_transform import DenseTransformer
+from dense_transform import DenseTransformer, tokenizer
 from matplotlib import pyplot
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfTransformer,  CountVectorizer, TfidfVectorizer
@@ -20,14 +20,35 @@ neg_dir = os.path.join(cur_dir, 'neg')
 
 
 # Load files names
-list_pos_dir = [ (os.path.join(pos_dir, x), 1) for x in os.listdir(pos_dir)][:1000]
-list_neg_dir = [ (os.path.join(neg_dir, x), 0) for x in os.listdir(neg_dir)][:1000]
+list_pos_dir = [ (os.path.join(pos_dir, x), 1) for x in os.listdir(pos_dir)][:100]
+list_neg_dir = [ (os.path.join(neg_dir, x), 0) for x in os.listdir(neg_dir)][:100]
 print("registers: {}".format(len(list_pos_dir+list_neg_dir)))
 print("Attention with 6000 registers it will consume about 5+GB of ram")
 # input("Continue? or press CTRL+C")
 
 # Mount data with label data frame
+
 paths_df = pandas.DataFrame(list_pos_dir+list_neg_dir, columns=['path', 'label'])
+
+# Verify difference between size of tokens with tokenizer stem, stopwords
+tfidf_stem = TfidfVectorizer(input='filename', stop_words='english', tokenizer=tokenizer)
+tfidf_stop = TfidfVectorizer(input='filename', stop_words='english')
+tfidf_word = TfidfVectorizer(input='filename')
+
+result = []
+
+for tfidf in [tfidf_stem, tfidf_word, tfidf_stop]:
+    tfidf.fit(paths_df.path.values)
+    result.append(len(tfidf.get_feature_names()))
+
+result = pandas.DataFrame(result, columns=['len_of_features'], index=['tfidf_stem', 'tfidf_word', 'tfidf_stop'])
+result = result.assign(difference=lambda x: (x.len_of_features - x.len_of_features.min()))
+print(result)
+pyplot.figure(3)
+pyplot.bar([1,2,3], result.difference.values)
+pyplot.xticks([1,2,3], result.index.values)
+pyplot.ylabel('Number of tokens')
+pyplot.xlabel('Method of tf-idf')
 
 pipes = {
     'gaussianNB': Pipeline([
@@ -45,6 +66,53 @@ pipes = {
       ( 'gnb', MultinomialNB())
     ])
 }
+
+def extract_params(best_params_):
+    return {'ngram_range': best_params_['vect__ngram_range'],
+      'use_idf': best_params_['vect__use_idf'],
+      'norm':  best_params_['vect__norm'],
+      'sublinear_tf': best_params_['vect__sublinear_tf'],
+      'stop_words': best_params_['vect__stop_words'],
+      'tokenizer': best_params_['vect__tokenizer']
+    }
+
+parameters = {
+  'vect__ngram_range': [(1,1), (1,2)],
+  'vect__use_idf': (True, False),
+  'vect__norm':  ('l2', 'l1', None),
+  'vect__sublinear_tf': (True, False),
+  'vect__stop_words': ('english', None),
+  'vect__tokenizer': (None, tokenizer),
+}
+
+parametrized = GridSearchCV(pipes['gaussianNB'], parameters, n_jobs=3)
+parametrized.fit(paths_df.path,paths_df.label)
+print(parametrized.best_score_, parametrized.best_params_)
+
+pipes['optimizedgaussianNB'] = Pipeline([
+      ('vect', TfidfVectorizer(input='filename', **extract_params(parametrized.best_params_))),
+      ('dense', DenseTransformer()),
+      ('gnb', GaussianNB())
+    ])
+
+parametrized = GridSearchCV(pipes['bernoulliNB'], parameters, n_jobs=3)
+parametrized.fit(paths_df.path,paths_df.label)
+print(parametrized.best_score_, parametrized.best_params_)
+
+pipes['optimizedbernoulliNB'] = Pipeline([
+      ('vect', TfidfVectorizer(input='filename', binary=True, **extract_params(parametrized.best_params_))),
+      ('dense', DenseTransformer()),
+      ('gnb', BernoulliNB())
+    ])
+
+parametrized = GridSearchCV(pipes['multinomialNB'], parameters, n_jobs=3)
+parametrized.fit(paths_df.path,paths_df.label)
+print(parametrized.best_score_, parametrized.best_params_)
+
+pipes['optimizedmultinomialNB'] = Pipeline([
+      ('vect', TfidfVectorizer(input='filename', **extract_params(parametrized.best_params_))),
+      ( 'gnb', MultinomialNB())
+    ])
 
 index = [x/10.0 for x in range(1,8)]
 
@@ -69,6 +137,7 @@ pyplot.subplot(221)
 pyplot.title('Gaussian NB')
 pyplot.plot(df.train_gaussianNB)
 pyplot.plot(df.test_gaussianNB)
+pyplot.plot(df.test_optimizedgaussianNB, 'r--')
 pyplot.ylabel("Score")
 pyplot.xlabel("% size train base")
 
@@ -76,6 +145,7 @@ pyplot.subplot(222)
 pyplot.title('BernoulliNB')
 pyplot.plot(df.train_bernoulliNB)
 pyplot.plot(df.test_bernoulliNB)
+pyplot.plot(df.test_optimizedbernoulliNB, 'r--')
 pyplot.ylabel("Score")
 pyplot.xlabel("% size train base")
 
@@ -84,6 +154,7 @@ pyplot.subplot(223)
 pyplot.title('MultinomialNB')
 pyplot.plot(df.train_multinomialNB)
 pyplot.plot(df.test_multinomialNB)
+pyplot.plot(df.test_optimizedmultinomialNB, 'r--')
 pyplot.ylabel("Score")
 pyplot.xlabel("% size train base")
 
@@ -95,7 +166,7 @@ remove_stopword = Pipeline([
       ('vect', TfidfVectorizer(input='filename', stop_words='english')),
       ('dense', DenseTransformer()),
       ('gnb', GaussianNB())
-    ])
+])
 
 temp = []
 for l in range(1,8):
@@ -122,6 +193,8 @@ pyplot.ylabel("Score")
 pyplot.xlabel("% size train base")
 
 pyplot.show()
+# #######TRASH CODE##################
+
 # Load text and transform into vector
 # cv = CountVectorizer(input='filename')
 # data = cv.fit_transform(paths_df['path'].values)
