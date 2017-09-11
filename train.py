5efe02a81bc8e02919830c7100b3b64c3c5a90a5
Marcelo Ferreira <marcelohpf@gmail.com>
2017-09-10 22:11:01 -0300

    Add validations to models and add SVM model
    Signed-off-by: Marcelo Ferreira <marcelohpf@gmail.com>

--- train.py
+++ train.py
@@ -6,7 +6,8 @@ from matplotlib import pyplot
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import TfidfTransformer,  CountVectorizer, TfidfVectorizer
 from sklearn.naive_bayes import GaussianNB, BernoulliNB, MultinomialNB
-from sklearn.svm import SVC
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import SGDClassifier 
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_validate
 from sklearn.model_selection import cross_val_score
@@ -20,8 +21,8 @@ neg_dir = os.path.join(cur_dir, 'neg')
 
 
 # Load files names
-list_pos_dir = [ (os.path.join(pos_dir, x), 1) for x in os.listdir(pos_dir)][:100]
-list_neg_dir = [ (os.path.join(neg_dir, x), 0) for x in os.listdir(neg_dir)][:100]
+list_pos_dir = [ (os.path.join(pos_dir, x), 1) for x in os.listdir(pos_dir)][:50]
+list_neg_dir = [ (os.path.join(neg_dir, x), 0) for x in os.listdir(neg_dir)][:50]
 print("registers: {}".format(len(list_pos_dir+list_neg_dir)))
 print("Attention with 6000 registers it will consume about 5+GB of ram")
 # input("Continue? or press CTRL+C")
@@ -64,7 +65,15 @@ pipes = {
     'multinomialNB': Pipeline([
       ('vect', TfidfVectorizer(input='filename')),
       ( 'gnb', MultinomialNB())
-    ])
+    ]),
+    'linearSVC': Pipeline([
+      ('vect', TfidfVectorizer(input='filename')),
+      ( 'gnb', LinearSVC())
+    ]),
+    'sgdclassifier': Pipeline([
+      ('vect', TfidfVectorizer(input='filename')),
+      ( 'gnb', SGDClassifier(max_iter=5))
+    ]),
 }
 
 def extract_params(best_params_):
@@ -114,6 +123,24 @@ pipes['optimizedmultinomialNB'] = Pipeline([
       ( 'gnb', MultinomialNB())
     ])
 
+# parametrized = GridSearchCV(pipes['linearSVC'], parameters, n_jobs=3)
+# parametrized.fit(paths_df.path,paths_df.label)
+# print(parametrized.best_score_, parametrized.best_params_)
+# 
+# pipes['optimizedlinearSVC'] = Pipeline([
+#       ('vect', TfidfVectorizer(input='filename', **extract_params(parametrized.best_params_))),
+#       ('gnb', LinearSVC())
+#     ])
+# 
+# parametrized = GridSearchCV(pipes['sgdclassifier'], parameters, n_jobs=3)
+# parametrized.fit(paths_df.path,paths_df.label)
+# print(parametrized.best_score_, parametrized.best_params_)
+# 
+# pipes['optimizedsgdclassifier'] = Pipeline([
+#       ('vect', TfidfVectorizer(input='filename', **extract_params(parametrized.best_params_))),
+#       ('gnb', SGDClassifier())
+#     ])
+
 index = [x/10.0 for x in range(1,8)]
 
 df = pandas.DataFrame(index=index)
@@ -133,6 +160,8 @@ for pipe_name, pipe in pipes.items():
     df = df.join(new_df)
 
 pyplot.figure(1)
+
+
 pyplot.subplot(221)
 pyplot.title('Gaussian NB')
 pyplot.plot(df.train_gaussianNB)
@@ -158,6 +187,23 @@ pyplot.plot(df.test_optimizedmultinomialNB, 'r--')
 pyplot.ylabel("Score")
 pyplot.xlabel("% size train base")
 
+pyplot.figure(4)
+pyplot.subplot(222)
+pyplot.title('LinearSVC')
+pyplot.plot(df.train_linearSVC)
+pyplot.plot(df.test_linearSVC)
+# pyplot.plot(df.test_optimizedlinearSVC, 'r--')
+pyplot.ylabel("Score")
+pyplot.xlabel("% size train base")
+
+
+pyplot.subplot(221)
+pyplot.title('SGDClassifier')
+pyplot.plot(df.train_sgdclassifier)
+pyplot.plot(df.test_sgdclassifier)
+# pyplot.plot(df.test_optimizedsgdclassifier, 'r--')
+pyplot.ylabel("Score")
+pyplot.xlabel("% size train base")
 pyplot.subplots_adjust(top=0.92, bottom=0.08, left=0.10, right=0.95, hspace=0.25,
                     wspace=0.35)
 print(df.ix[df.idxmax()])
